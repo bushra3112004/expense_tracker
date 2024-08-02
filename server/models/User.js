2452be849d24a5b20bb4dd91c25bbbe381cde8e5
bushra3112004 <pathanbushra006@gmail.com>
2024-08-02 16:39:49 -0700

    add server code

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+import { Schema,model } from "mongoose";
+
+const userSchema = new Schema({
+    fullName:{
+        type:String,
+       required:true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+},{
+   timestamps:true,
+});
+
+const user = model("User",userSchema)
